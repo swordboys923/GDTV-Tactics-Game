178eeda8b8d3ab8866235039508ceedb87c5329e
swordboys923 <tdonahue630@gmail.com>
2022-12-28 22:33:20 -0500

    --Created a Resolve and Stamina system (mirroring the HealthSystem.cs class) to begin working on a Total War style system of fatigue and breaking morale

---
+++ Assets/Scripts/ResolveSystem.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResolveSystem : MonoBehaviour {
+
+    // public static event EventHandler OnAnyResolveChanged;
+    public event EventHandler OnResolveBroken;
+    public event EventHandler OnResolveChanged;
+    [SerializeField] private int resolve = 100;
+    private int resolveMax;
+
+    private void Awake() {
+        resolveMax = resolve;
+    }
+
+    public void LoseResolve(int lossAmount) {
+        resolve -= lossAmount;
+        if(resolve < 0) {
+            resolve = 0;
+        }
+        OnResolveChanged?.Invoke(this, EventArgs.Empty);
+        // OnAnyResolveChanged?.Invoke(this,EventArgs.Empty);
+        
+        if (resolve == 0) {
+            Break();
+        }
+    }
+
+    private void Break() {
+        OnResolveBroken?.Invoke(this,EventArgs.Empty);
+    }
+
+    public float GetResolveNormalized() {
+        return (float)resolve / resolveMax;
+    }
+
+    public int GetResolve() {
+        return resolve;
+    }
+
+    public int GetResolveMax() {
+        return resolveMax;
+    }
+}
---
+++ Assets/Scripts/StaminaSystem.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StaminaSystem : MonoBehaviour{
+
+    // public static event EventHandler OnAnyStaminaChanged;
+    public event EventHandler OnStaminaDrained;
+    public event EventHandler OnStaminaChanged;
+    [SerializeField] private int stamina = 100;
+    private int staminaMax;
+
+    private void Awake() {
+        staminaMax = stamina;
+    }
+
+    public void LoseStamina(int lossAmount) {
+        stamina -= lossAmount;
+        if(stamina < 0) {
+            stamina = 0;
+        }
+        OnStaminaChanged?.Invoke(this, EventArgs.Empty);
+        // OnAnyStaminaChanged?.Invoke(this,EventArgs.Empty);
+        
+        if (stamina == 0) {
+            Exhaust();
+        }
+    }
+
+    private void Exhaust() {
+        OnStaminaDrained?.Invoke(this,EventArgs.Empty);
+    }
+
+    public float GetStaminaNormalized() {
+        return (float)stamina / staminaMax;
+    }
+
+    public int GetStamina() {
+        return stamina;
+    }
+
+    public int GetStaminaMax() {
+        return staminaMax;
+    }
+}
