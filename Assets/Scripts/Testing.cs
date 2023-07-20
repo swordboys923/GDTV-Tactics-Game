9029e30d0494602d14f1926517568637bf6e3c78
swordboys923 <tdonahue630@gmail.com>
2023-07-19 21:09:23 -0400

    --Attempting to rig up a stamina system mock UI.

--- Assets/Scripts/Testing.cs
+++ Assets/Scripts/Testing.cs
@@ -5,57 +5,11 @@ using UnityEngine;
 
 public class Testing : MonoBehaviour {
 
-    [SerializeField] ParticleSystem particleEffect;
-    [SerializeField] int range;
-    [SerializeField] Unit unit;
-    List<GridPosition> gridPositionList;
-    AOESpellAction fireAction;
-
-    private void Start() {
-        foreach(BaseAction baseAction in unit.GetBaseActionArray()) {
-            if(baseAction is AOESpellAction) {
-                AOESpellAction fireAction = (AOESpellAction) baseAction;
-                fireAction.OnSpellCharging += FireAction_OnSpellCharging;
-                fireAction.OnSpellCasting += FireAction_OnSpellCasting;
-                fireAction.OnSpellCooling += FireAction_OnSpellCooling;
-            }
-
-        }
-    }
-
-    private void FireAction_OnSpellCooling(object sender, EventArgs e)
-    {
-        Debug.Log("Cooling from Event!");
-    }
-
-    private void FireAction_OnSpellCasting(object sender, EventArgs e)
-    {
-        Debug.Log("Casting from Event!");
-    }
-
-    private void FireAction_OnSpellCharging(object sender, EventArgs e)
-    {
-        Debug.Log("Charging from Event!");
-    }
+    [SerializeField] UnitWorldUI unitWorldUI;
 
     private void Update() {
         if(Input.GetKeyDown(KeyCode.T)) {
-            GridAnimationManager.Instance.SpawnEffect(GetGridPositionRadius(), particleEffect);
+            unitWorldUI.StaminaSystem_OnStaminaChanged();
         }
-    }
-
-    private GridPosition[] GetGridPositionRadius(){
-        gridPositionList = new List<GridPosition>();
-        Unit unit = TurnManager.Instance.GetCurrentTurnUnit();
-        GridPosition unitGridPosition = unit.GetGridPosition();
-        for (int x = -range; x <= range; x++) {
-            for (int z = -range; z <= range; z++) {
-                GridPosition gridPosition = new GridPosition(x,z) + unitGridPosition;
-                if(LevelGrid.Instance.IsValidGridPosition(gridPosition)){
-                    gridPositionList.Add(gridPosition);
-                }
-            }
-        }
-        return gridPositionList.ToArray();
     }
 }
--- Assets/Scripts/UI/UnitWorldUI.cs
+++ Assets/Scripts/UI/UnitWorldUI.cs
@@ -7,10 +7,16 @@ public class UnitWorldUI : MonoBehaviour {
     [SerializeField] private Unit unit;
     [SerializeField] private Image healthBarImage;
     [SerializeField] private HealthSystem healthSystem;
+    [SerializeField] private Image staminaImage;
+    [SerializeField] private Color[] staminaImageColorArray;
+    int staminaIndex = 0;
 
     private void Start() {
         healthSystem.OnHealthChanged += HealthSystem_OnHealthChanged;
+
         UpdateHealthBar();
+        staminaIndex = 0;
+        staminaImage.color = staminaImageColorArray[staminaIndex];
     }
 
     private void UpdateHealthBar() {
@@ -19,5 +25,11 @@ public class UnitWorldUI : MonoBehaviour {
 
     private void HealthSystem_OnHealthChanged(object sender, EventArgs e) {
         UpdateHealthBar();
+    }
+
+    public void StaminaSystem_OnStaminaChanged(/*object sender, EventArgs e*/) {
+        if (staminaIndex == 2) return;
+        staminaIndex++;
+        staminaImage.color = staminaImageColorArray[staminaIndex];
     }
 }
