2e63153922ededf4ad1113a8010d4b00907bde09
swordboys923 <>
2023-08-25 23:59:18 -0400

    --Started to set up popup damage numbers when health changes --Spent a lot of time fighting with VSCode for intellisense (failed)

--- Assets/Scripts/UI/UnitWorldUI.cs
+++ Assets/Scripts/UI/UnitWorldUI.cs
@@ -1,10 +1,12 @@
 using System;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class UnitWorldUI : MonoBehaviour {
     [SerializeField] private Image healthBarImage;
     [SerializeField] private HealthSystem healthSystem;
+    [SerializeField] private TextMeshProUGUI healthText;
 
     private void Start() {
         healthSystem.OnHealthChanged += HealthSystem_OnHealthChanged;
