a75e4343ee5152e89dcb3345d7b983e963cf8fee
swordboys923 <tdonahue630@gmail.com>
2023-08-28 16:00:46 -0400

    --Started to look at animating the popup numbers

--- Assets/Scripts/UI/UnitWorldUI.cs
+++ Assets/Scripts/UI/UnitWorldUI.cs
@@ -6,9 +6,12 @@ using TMPro;
 public class UnitWorldUI : MonoBehaviour {
     [SerializeField] private Image healthBarImage;
     [SerializeField] private HealthSystem healthSystem;
-    [SerializeField] private TextMeshPro healthText;
+    [SerializeField] private TextMeshPro healthNumberText;
+    private Transform healthNumberTextTransform;
+    private bool isActive;
 
     private void Start() {
+        healthNumberTextTransform = healthNumberText.transform;
         healthSystem.OnHealthChanged += HealthSystem_OnHealthChanged;
         UpdateHealthBar();
     }
@@ -19,5 +22,9 @@ public class UnitWorldUI : MonoBehaviour {
 
     private void HealthSystem_OnHealthChanged(object sender, EventArgs e) {
         UpdateHealthBar();
+    }
+
+    private void DisplayHealthNumbers(int amount) {
+        isActive = true;
     }
 }
